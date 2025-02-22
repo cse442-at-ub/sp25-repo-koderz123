0515e6ae3f8d97c4b5a6cb0746db7bbff94a878c
Ray Duan <raymondduan02@gmail.com>
2025-02-22 16:48:20 -0500

    removed unused React module from 'react' in first header line

--- Koderz123/src/OptionScreenComponents/OptionScreen.tsx
+++ Koderz123/src/OptionScreenComponents/OptionScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState , ChangeEvent , MouseEvent} from 'react';
+import { useState , ChangeEvent , MouseEvent} from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./OptionScreen.css";
 

bbbaf4e5b2768139dd80cada88f3c8d76c1aac60
mashfiqueahmed <mashfiq.mohi@gmail.com>
2025-02-23 19:44:34 -0500

    ready for testing
    getting ready to test to see how it looks

--- Koderz123/src/MainMenuComponents/MainMenu.tsx
+++ Koderz123/src/MainMenuComponents/MainMenu.tsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { MouseEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import "./MainMenu.css";
 
 const MainMenu: React.FC = () => {
-  return (
-    <>
-      <h1>GALACTIC TOWER DEFENSE</h1>
-      <div className="menu-item">New Game</div>
-      <div className="menu-item">Options</div>
-      <div className="menu-item">Exit</div>
-    </>
-  );
+    const navigate = useNavigate(); // Initialize navigation
+
+    const clickNewGame = () => {
+        navigate("/difficulty");
+    };
+
+    const clickOptions = () => {
+        navigate("/options");
+    };
+
+    const clickExit = () => {
+        navigate("/landing");
+    };
+
+    const hovering = (e: MouseEvent<HTMLElement>) => {
+        (e.target as HTMLElement).style.fontSize = "38px";
+        e.currentTarget.style.cursor = "pointer";
+    };
+
+    const notHovering = (e: MouseEvent<HTMLElement>) => {
+        (e.target as HTMLElement).style.fontSize = "36px";
+        e.currentTarget.style.cursor = "default";
+    };
+
+    return (
+        <div id="MainMenupage">
+            <h1>GALACTIC TOWER DEFENSE</h1>
+            <div 
+                className="menu-item" 
+                onClick={clickNewGame} 
+                onMouseEnter={hovering} 
+                onMouseLeave={notHovering}
+            >
+                NEW GAME
+            </div>
+            <div 
+                className="menu-item" 
+                onClick={clickOptions} 
+                onMouseEnter={hovering} 
+                onMouseLeave={notHovering}
+            >
+                OPTIONS
+            </div>
+            <div 
+                className="menu-item" 
+                onClick={clickExit} 
+                onMouseEnter={hovering} 
+                onMouseLeave={notHovering}
+            >
+                EXIT
+            </div>
+        </div>
+    );
 };
 
 export default MainMenu;

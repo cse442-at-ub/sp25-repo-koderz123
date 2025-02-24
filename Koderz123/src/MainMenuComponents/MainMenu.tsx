5bd0462e75cef0e440c060429cf8b1f76d6e3420
mashfiqueahmed <mashfiq.mohi@gmail.com>
2025-02-23 19:10:02 -0500

    made the typescipt file
    as summary

--- Koderz123/src/MainMenuComponents/MainMenu.tsx
+++ Koderz123/src/MainMenuComponents/MainMenu.tsx
@@ -1,2 +1,15 @@
+import React from 'react';
+import "./MainMenu.css";
+
+const MainMenu: React.FC = () => {
+  return (
+    <>
+      <h1>GALACTIC TOWER DEFENSE</h1>
+      <div className="menu-item">New Game</div>
+      <div className="menu-item">Options</div>
+      <div className="menu-item">Exit</div>
+    </>
+  );
+};
 
 export default MainMenu;

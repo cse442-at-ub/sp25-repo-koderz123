f3a7ebc3a8b969bc5df4014ff9edfb6b1524da85
Ray Duan <raymondduan02@gmail.com>
2025-03-02 17:12:09 -0500

    Setting up components for the login screen

---
+++ Koderz123/src/LoginScreenComponents/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./LoginScreen.css";
+
+const LoginScreen: React.FC = () => {
+    return (
+        <div id = "loginpage">
+            
+        </div>
+    )
+};
+
+export default LoginScreen;

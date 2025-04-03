d0c2893c8aaace1e2539ff01ea880c53338d999f
mashfiqueahmed <mashfiq.mohi@gmail.com>
2025-04-03 18:10:27 -0400

    deleted some redundant files
    I tried adding a few things but these were not required at all. So I am removing these for a cleanup commit.

--- Koderz123/src/App.tsx
+++ Koderz123/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import React, { useEffect } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LevelSelect from "./DifficultyScreenComponents/DifficultyScreen";
 import OptionScreen from "./OptionScreenComponents/OptionScreen";
 import MainMenu from "./MainMenuComponents/MainMenu";
@@ -11,68 +11,41 @@ import InitialGame from "./PhaserGameFiles/InitialGame";
 import TutorialScreen from "./TutorialScreenComponents/TutorialScreen";
 import Leaderboard from "./LeaderboardComponents/Leaderboard";
 import ProtectedRoute from "./ProtectedRoute.tsx";
-import Login from "./components/Login";
-import Game from "./components/Game";
-import Navbar from "./components/Navbar";
-import "./App.css";
 
 const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState("");
-
-  const handleLogin = (username: string) => {
-    setIsLoggedIn(true);
-    setUsername(username);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setUsername("");
-  };
+  useEffect(() => {
+    // Comment out login check to bypass authentication
+    // const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+    // if (!isLoggedIn) {
+    //   navigate("/");
+    // }
+  }, []);
 
   return (
     <MusicProvider>
       <Router basename="">
         <div className="container mx-auto p-4">
-          <Navbar isLoggedIn={isLoggedIn} username={username} onLogout={handleLogout} />
           <Routes>
-            <Route
-              path="/"
-              element={
-                isLoggedIn ? (
-                  <Navigate to="/game" replace />
-                ) : (
-                  <Login onLogin={handleLogin} />
-                )
-              }
-            />
-            <Route
-              path="/game"
-              element={
-                isLoggedIn ? (
-                  <Game username={username} />
-                ) : (
-                  <Navigate to="/" replace />
-                )
-              }
-            />
-            <Route
-              path="/leaderboard"
-              element={
-                isLoggedIn ? (
-                  <Leaderboard />
-                ) : (
-                  <Navigate to="/" replace />
-                )
-              }
-            />
-            <Route path="/levelselect" element={<LevelSelect />} />
-            <Route path="/mainmenu" element={<MainMenu />} />
-            <Route path="/gamescreen" element={<InitialGame />} />
-            <Route path="/options" element={<OptionScreen />} />
-            <Route path="/PhaserHelloWorld" element={<PhaserHelloWorld />} />
-            <Route path="/tutorial" element={<TutorialScreen />} />
-            <Route path="*" element={<div>404 - Not Found</div>} />
+            <Route path="/" element={<LandingPage />} />{" "}
+            {/* replace the html code with landing page component*/}
+            <Route path="/login" element={<LoginScreen />} />
+            {/* Any links that should be protected and only accessible via login should be placed   inside
+            the below element*/}
+            {/* <Route element={<ProtectedRoute />}> */}
+              <Route path="/levelselect" element={<LevelSelect />} />{" "}
+              {/* replace the html code with difficulty page component */}
+              <Route path="/mainmenu" element={<MainMenu />} />{" "}
+              {/*replace the html code with main menu component*/}
+              <Route path="/gamescreen" element={<InitialGame />} />{" "}
+              {/* replace the html code with gamescreen component */}
+              <Route path="/options" element={<OptionScreen />} />{" "}
+              {/* replace the html code with options component */}
+              <Route path="/PhaserHelloWorld" element={<PhaserHelloWorld />} />
+              <Route path="/tutorial" element={<TutorialScreen />} />
+              <Route path="/leaderboard" element={<Leaderboard />} />
+            {/* </Route> */}
+            <Route path="*" element={<div>404 - Not Found</div>} />{" "}
+            {/* the star represents a catch all so anything that we havent routed out goes to 404 page */}
           </Routes>
         </div>
       </Router>
